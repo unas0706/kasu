37451758ad2262d48ec8b554e7eb7f05ac88c2c4
unas0706 <sk.unas 123@gmail.com>
2026-01-25 20:35:48 +0530

    updated cors

--- kasu-backend/server.js
+++ kasu-backend/server.js
@@ -18,7 +18,11 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // later replace with your frontend URL
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
