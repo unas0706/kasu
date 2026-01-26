c87a9f7f2d6ad99dc85c837b18c975ce90bbfa1e
unas0706 <sk.unas 123@gmail.com>
2026-01-26 22:37:30 +0530

    updated liveorders socket

--- kasu-frontend/src/components/sockets/socket.js
+++ kasu-frontend/src/components/sockets/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://kasu-4z4t.onrender.com");
+// const socket = io("http://localhost:5000");
 
 export default socket;
